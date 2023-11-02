1e7610259fb957cbe628b65e1b41322de117622a
AymanGhazal1997 <149606335+AymanGhazal1997@users.noreply.github.com>
2023-11-02 20:11:23 +0100

    Update Livre.scala

--- src/main/scala/Livre.scala
+++ src/main/scala/Livre.scala
@@ -1,14 +1,15 @@
 class Livre (var titre: String, var auteur: String, var anneeDePublication: Int, var estEmprunte: Boolean) {
+    // Méthode pour emprunter un livre
   def emprunter(): Unit = {
     estEmprunte = true
   }
-
+ // Méthode pour rendre un livre
   def rendre(): Unit = {
     estEmprunte = false
   }
-
+// Redéfinir la méthode toString pour afficher les détails du livre
   override def toString: String = s"$titre;$auteur;$anneeDePublication;$estEmprunte"
-
+// Redéfinir la méthode equals pour comparer deux livres
   override def equals(other: Any): Boolean = other match {
     case that: Livre =>
       this.titre == that.titre &&
